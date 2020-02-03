0acf2161716702ea427bc5f2256d8b82ba49c544
tatialveso <tatiialveso@gmail.com>
2020-02-03 09:15:13 -0300

    Criação de novas rotas

--- routes/web.php
+++ routes/web.php
@@ -15,58 +15,59 @@
 Route::get('/', 'HomeController@index');
 
 // Página inicial logado
-Route::get('/perfil/{id}', 'PerfilController@show');
+Route::get('/perfil/{id}', 'PerfilController@index');
 
 // Página da loja
-Route::get('/loja/{id}', 'LojaController@show');
+Route::get('/loja/{id}', 'LojaController@index');
 
 // Página do produto
-Route::get('/produto/{id}', 'ProdutoController@show');
+Route::get('/produto/{id}', 'ProdutoController@index');
 
 // Quem Somos
-Route::get('/sobre', 'SobreController@show');
+Route::get('/sobre', 'SobreController@index');
 
 // Como funciona
-Route::get('/como-funciona', 'ComoController@show');
+Route::get('/como-funciona', 'ComoController@index');
 
 // Contato
-Route::get('/contato', 'ContatoController@show');
+Route::get('/contato', 'ContatoController@index');
 
 // Cadastro de usuário
-Route::get('/cadastro', 'CadastroController@show');
+Route::get('/cadastro', 'CadastroController@create');
+Route::post('cadastro/salvo', 'CadastroController@store');
 
 // Login do usuário
-Route::get('/login', 'LoginController@show');
+Route::get('/login', 'LoginController@index');
 
 // Incluir produto
-Route::get('/incluir-produto','IncluirController@show');
+Route::get('/incluir-produto','IncluirController@index');
 
 // Catálogo de higiene pessoal
-Route::get('/higiene-pessoal', 'PessoalController@show');
+Route::get('/higiene-pessoal', 'PessoalController@index');
 
 // Catálogo de casa e ambiente
-Route::get('/casa-ambiente', 'CasaController@show');
+Route::get('/casa-ambiente', 'CasaController@index');
 
 // Favoritos
-Route::get('/favoritos', 'FavoritoController@show');
+Route::get('/favoritos', 'FavoritoController@index');
 
 // Configurações de dados do usuário
-Route::get('/configuracoes', 'ConfiguracoesController@show');
+Route::get('/configuracoes', 'ConfiguracoesController@index');
 
 // Segurança de dados do usuário
-Route::get('/seguranca', 'SegurancaController@show');
+Route::get('/seguranca', 'SegurancaController@index');
 
 // Histórico de vendas
-Route::get('/historico-vendas', 'VendaController@show');
+Route::get('/historico-vendas', 'VendaController@index');
 
 // Histórico de compras
-Route::get('/historico-compras', 'CompraController@show');
+Route::get('/historico-compras', 'CompraController@index');
 
 // Carrinho de compras
-Route::get('/carrinho', 'CarrinhoController@show');
+Route::get('/carrinho', 'CarrinhoController@index');
 
 // Checkout de compras
-Route::get('/checkout', 'CheckoutController@show');
+Route::get('/checkout', 'CheckoutController@index');
 
 // Resumo do pedido
-Route::get('/resumo-pedido', 'ResumoController@show');
+Route::get('/resumo-pedido', 'ResumoController@index');
