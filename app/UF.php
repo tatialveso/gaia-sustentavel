0d65caa7faaffd6c33f92cbd737ee5b4d3ab7483
tatialveso <tatiialveso@gmail.com>
2020-02-17 15:56:13 -0300

    Modificações no model UF

--- app/UF.php
+++ app/UF.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class UF extends Model
 {
+    protected $table = "ufs";
     public function user() {
         return $this->hasMany('app\User', 'user_id', 'id');
     }
